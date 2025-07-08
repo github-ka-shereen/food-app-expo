1af2c90d25f3dfacda0b1afc57a71c849309c345
Shereni <info@aewebsites.co.zw>
2025-07-08 20:26:59 +0200

    Feature: Index page no bottom nav

--- app/index.tsx
+++ app/index.tsx
@@ -1,32 +1,84 @@
 // index.tsx
-import { Text, View } from 'react-native';
+import CartButton from '@/components/CartButton';
+import { images, offers } from '@/constants';
+import cn from 'clsx';
+import {
+  FlatList,
+  Image,
+  Pressable,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Index() {
   return (
-    <View className='flex-1 items-center justify-center bg-white'>
-      {/* Your Nativewind class usage (keep this for now) */}
-      <Text className='text-4xl text-center font-quicksand-bold text-blue-500'>
-        Welcome to Nativewind
-      </Text>
-
-      {/* ADD THIS NEW TEXT COMPONENT */}
-      <Text
-        style={{
-          fontFamily: 'QuickSand-Bold',
-          fontSize: 24,
-          color: 'red',
-          marginTop: 20,
+    <SafeAreaView className='flex-1 bg-white'>
+      <FlatList
+        data={offers}
+        renderItem={({ item, index }) => {
+          const isEven: boolean = index % 2 === 0;
+          return (
+            <View>
+              <Pressable
+                className={cn(
+                  'offer-card',
+                  isEven ? 'flex-row-reverse' : 'flex-row'
+                )}
+                style={{ backgroundColor: item.color }}
+                android_ripple={{ color: '#ffffff22' }}
+              >
+                {({ pressed }) => (
+                  <>
+                    <View className='h-full w-1/2'>
+                      <Image
+                        source={item.image}
+                        className='size-full'
+                        resizeMode='cover'
+                      />
+                    </View>
+                    <View
+                      className={cn(
+                        'offer-card__info',
+                        isEven ? 'pl-10' : 'pr-10'
+                      )}
+                    >
+                      <Text className='h1-bold text-white leading-tight'>
+                        {item.title}
+                      </Text>
+                      <Image
+                        source={images.arrowRight}
+                        className='size-10'
+                        resizeMode='contain'
+                        tintColor={'#ffffff'}
+                      />
+                    </View>
+                  </>
+                )}
+              </Pressable>
+            </View>
+          );
         }}
-      >
-        This text uses direct style!
-      </Text>
+        contentContainerClassName='pb-28 px-5'
+        ListHeaderComponent={() => (
+          <View className='flex-between flex-row w-ful my-5'>
+            <View className='flex-start'>
+              <Text className='small-bold text-primary'>DELIVER TO</Text>
 
-      {/* You can also try other weights directly if you want */}
-      <Text
-        style={{ fontFamily: 'QuickSand-Regular', fontSize: 20, color: 'blue' }}
-      >
-        Direct Style - Regular
-      </Text>
-    </View>
+              <TouchableOpacity className='flex-center flex-row gap-x-1 mt-0.5'>
+                <Text className='paragraph-bold text-dark-100'>Zimbabwe</Text>
+                <Image
+                  source={images.arrowDown}
+                  className='size-3'
+                  resizeMode='contain'
+                />
+              </TouchableOpacity>
+            </View>
+            <CartButton />
+          </View>
+        )}
+      />
+    </SafeAreaView>
   );
 }
